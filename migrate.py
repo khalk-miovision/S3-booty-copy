0d3e70a2b82ddd45299ccc8dc22ad78174fcc63b
nxsyed <nxsyed.ahmed@gmail.com>
2019-10-29 14:32:08 -0400

    Read from file

--- migrate.py
+++ migrate.py
@@ -1,14 +1,23 @@
 import boto3
+import csv
+
 
 client = boto3.client('sts')
 
 token = str(input("Enter your token: "))
 
 response = client.assume_role(
-    RoleArn='OrganizationAccountAccessRole-Developer',
+    RoleArn='arn:aws:iam::751075880680:role/OrganizationAccountAccessRole-Developer',
     RoleSessionName='@S3migration@',
     SerialNumber='arn:aws:iam::457774690605:mfa/sahmed',
     TokenCode=token
 )
 
-print(response)
+s3 = boto3.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+# with open('copy_paths.csv') as csv_file:
+#     logos = csv.reader(csv_file, delimiter=',')
+#     for path in logos:
+#         print(path[0])
